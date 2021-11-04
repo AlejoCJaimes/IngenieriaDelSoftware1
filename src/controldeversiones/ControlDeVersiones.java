94e81aa7cf238687dfc62be2c34ffc8d8b0ee25f
Alejandro Jaimes <61189108+AlejoCJaimes@users.noreply.github.com>
2021-11-04 14:28:51 -0500

    version2

--- src/controldeversiones/ControlDeVersiones.java
+++ src/controldeversiones/ControlDeVersiones.java
@@ -16,6 +16,8 @@ public class ControlDeVersiones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+          System.out.println("Version 1");
+        
     }
     
 }
